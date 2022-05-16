293d446cd77ac07bf925b156eebd2507bbc4ce6a
Mikaila Smith <mikaila.a.smith@gmail.com>
2022-05-15 23:38:57 -0400

    Create Library app for Module 4 Lesson 15 Wrap up Challenge

---
+++ Library/Library/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by Mikaila Smith on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

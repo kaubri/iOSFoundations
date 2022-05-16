1e922e04ef5f3105f6ae333569e9d574bb00c772
Mikaila Smith <mikaila.a.smith@gmail.com>
2022-05-16 18:18:54 -0400

    Learning app module 5

---
+++ SwiftLearning/SwiftLearning/SwiftLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearningApp.swift
+//  SwiftLearning
+//
+//  Created by Mikaila Smith on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

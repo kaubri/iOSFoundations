2801dc8bf066f23acbed26ee0b40fc71a92ed241
Mikaila Smith <mikaila.a.smith@gmail.com>
2022-05-21 20:17:36 -0400

    add assets, add data files, add models, parse json and style

--- SwiftLearning/SwiftLearning/SwiftLearningApp.swift
+++ SwiftLearning/SwiftLearning/SwiftLearningApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct SwiftLearningApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
---
+++ SwiftLearning/SwiftLearning/ViewModels/ContentModel.swift
@@ -0,0 +1,44 @@
+//
+//  ContentModel.swift
+//  SwiftLearning
+//
+//  Created by Mikaila Smith on 5/16/22.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    @Published var modules = [Module]()
+    var styleData:Data?
+    
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            let jsonDecoder = JSONDecoder()
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            self.modules = modules
+        }
+        catch {
+            print("Couldn't parse local data")
+        }
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            let styleData = try Data(contentsOf: styleUrl!)
+            self.styleData = styleData
+            
+        }
+        catch {
+            print("Couldn't parse style data")
+        }
+    }
+}
